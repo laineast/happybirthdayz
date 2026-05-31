9c1b221973ec7eeddd5078fb47818b7b9e5f7578
laineast <stellajanemaquiling@gmail.com>
2026-06-01 07:44:52 +0800

    Update music file path in config.js

--- config.js
+++ config.js
@@ -29,7 +29,7 @@ const CONFIG = {
   // ── Recipient Info ────────────────────────────────────────────
   name: "Irene",
   photo: "img/received_962041532826799.jpeg",       // Place your photo in the img/ folder
-  music: "./music/hbd.mpeg",      // Place your music in the music/ folder
+  music: "YouCut_20260601_074332450.mp4",      // Place your music in the music/ folder
 
   // ── Theme Colors ──────────────────────────────────────────────
   // A toggle button lets the viewer switch between dark & light mode.
